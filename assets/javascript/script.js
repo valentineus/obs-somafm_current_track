070bf44c47547729d8a1f975dcb3f52a865b7588
Valentin Popov <info@valentineus.link>
2018-05-31 07:10:45 +0400

    Refresh the page title
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -34,7 +34,6 @@ function updateData(artistTrack, titleTrack) {
 
   if (artistElement.textContent !== artistTrack || titleElement.textContent !== titleTrack) {
     /* Updates text */
-    document.title = artistTrack + ' - ' + titleTrack;
     artistElement.textContent = artistTrack;
     titleElement.textContent = titleTrack;
 
